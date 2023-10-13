607c030b361d6f9c31153cfef04cbcf3bd2be648
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-10-12 19:44:03 -0700

    Add API, build load of API and update of state.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
 import './App.css';
+import { getDefaultNormalizer } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      monsters: [],
+    }
+    console.log('constructor')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState(() => {
+        return { monsters: users }
+      },
+        () => { console.log("state", this.state) }
+      ))
+  }
+  
+  render() {
+    console.log('render')
+    return (
+      <div className="App">
+
+        <input className='search-box' type='search' placeholder='search monsters' />
+
+        {
+          this.state.monsters.map((monster => {
+            return <h1 key={monster.id}>{monster.name}</h1>
+        }))
+        }
+
+        </div>
+      )
+    }
 }
-
+  
 export default App;
