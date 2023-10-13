4782b8bc1fc441941a2c93dc790fa8335666e32a
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-10-13 13:48:23 -0700

    Optimize by creating a method for search; optimize by destructuring this.state and this

--- src/App.js
+++ src/App.js
@@ -1,60 +1,65 @@
-import { Component } from 'react';
+import { Component } from "react";
 
-import './App.css';
-import { getDefaultNormalizer } from '@testing-library/react';
+import "./App.css";
+import { getDefaultNormalizer } from "@testing-library/react";
 
 class App extends Component {
-
   constructor() {
-    super()
+    super();
 
     this.state = {
       persons: [],
-      searchField: '',
-    }
-    console.log('constructor')
+      searchField: "",
+    };
+    console.log("constructor");
   }
 
   componentDidMount() {
-    console.log('componentDidMount')
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState(() => {
-        return { persons: users }
-      }
-      ))
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { persons: users };
+        })
+      );
   }
-  
+
+
+  onSearchChange = (event) => {
+    const searchValue = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField: searchValue };
+    });
+  }
+
+
   render() {
-    console.log('render')
+    console.log("render");
 
+    const { persons, searchField } = this.state
+    const { onSearchChange } = this
 
-    const filteredPeople = this.state.persons.filter((person) => {
-      return person.name.toLocaleLowerCase().includes(this.state.searchField)
-    })
-   
+    const filteredPeople = persons.filter((person) => {
+      return person.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-
-        <input className='search-box' type='search' placeholder='search monsters' id='search-box' onChange={(event) => {
-          const searchValue = event.target.value.toLocaleLowerCase()
-          this.setState(
-            () => {
-              return { searchField: searchValue }
-            }
-          )
-        }}
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          id="search-box"
+          onChange={ onSearchChange }
         />
 
-        {
-          filteredPeople.map((person => {
-            return <h1 key={person.id}>{person.name}</h1>
-        }))
-        }
-
-        </div>
-      )
-    }
+        {filteredPeople.map((person) => {
+          return <h1 key={person.id}>{person.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
-  
+
 export default App;
